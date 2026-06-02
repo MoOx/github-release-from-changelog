d2b8db462e237ab8983ac094ba82f467779498ef
Max <git@moox.io>
2026-06-02 11:01:44 +0200

    migrate to grizzly 6 (ESM, Node >=18) (#30)
    Upgrade grizzly to ^6 to fix the @octokit/* ReDoS advisories (now 0 production vulnerabilities). grizzly 6 is ESM-only and requires Node >=18, so the CLI is migrated to ESM and the package is marked "type": "module". CLI usage (command, flags, output) is unchanged.
    
    Also replace prettier + pretty-quick with oxlint + oxfmt, removing the cross-spawn/execa advisories from the dev dependencies.
    
    Update husky

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # github-release-from-changelog
 
-[![Test Status](https://github.com/MoOx/github-release-from-changelog/workflows/Test/badge.svg)](https://github.com/MoOx/github-release-from-changelog/actions)
-[![Version](https://img.shields.io/npm/v/github-release-from-changelog.svg?style=flat)](https://www.npmjs.com/package/github-release-from-changelog)
+<a href="https://github.com/MoOx/react-from-svg?sponsor=1">
+  <img width="140" align="right" alt="Sponsoring button" src="https://github.com/moox/.github/raw/main/FUNDING.svg">
+</a>
+
+[![npm package version](https://img.shields.io/github/package-json/v/MoOx/github-release-from-changelog) ![npm downloads](https://img.shields.io/npm/dm/github-release-from-changelog)](https://www.npmjs.com/package/github-release-from-changelog)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/MoOx/github-release-from-changelog/test.yml)](https://github.com/MoOx/github-release-from-changelog/actions)
+[![License](https://img.shields.io/github/license/MoOx/github-release-from-changelog)](https://github.com/MoOx/github-release-from-changelog)  
+![My website moox.io](https://img.shields.io/badge/%F0%9F%8C%90-https%3A%2F%2Fmoox.io-gray?style=social)
+[![GitHub followers](https://img.shields.io/github/followers/MoOx?style=social&label=GitHub)](https://github.com/MoOx)
+[![LinkedIn Follow](https://img.shields.io/badge/%F0%9F%91%94-LinkedIn-gray?style=social&link=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fmaxthirouin%2F)](https://www.linkedin.com/in/maxthirouin/)
+[![BlueSky Follow](https://img.shields.io/badge/BlueSky-%20?style=social&logo=bluesky)](https://bsky.app/profile/moox.io)
+[![X Follow](https://img.shields.io/twitter/follow/MoOx?style=social&label=)](https://x.com/MoOx)
 
 > Create GitHub releases from `CHANGELOG.md`
 
--- github-release-from-changelog.js
+++ github-release-from-changelog.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
-var changelogFileNames = [
+import cp from "node:child_process";
+import fs from "node:fs";
+import process from "node:process";
+import minimist from "minimist";
+import grizzly from "grizzly";
+
+const changelogFileNames = [
   "CHANGELOG.md",
   "Changelog.md",
   "changelog.md",
@@ -15,7 +21,7 @@ var changelogFileNames = [
   "news.md",
   "RELEASES.md",
   "Releases.md",
-  "releases.md"
+  "releases.md",
 ];
 
 /**
@@ -35,71 +41,60 @@ var changelogFileNames = [
  */
 
 // read command line arguments
-var cp = require("child_process");
-var minimist = require("minimist");
-var argv = minimist(process.argv.slice(2));
-
-var fs = require("fs");
+const argv = minimist(process.argv.slice(2));
 
 // changelog file name
-var changelogFileName = argv.filename;
+let changelogFileName = argv.filename;
 if (!changelogFileName) {
-  for (var fileName of changelogFileNames) {
+  for (const fileName of changelogFileNames) {
     if (fs.existsSync(fileName)) {
       changelogFileName = fileName;
       break;
     }
   }
 }
-// console.log("changelog filename", changelogFileName);
-
-// get dep
-var release = require("grizzly");
 
 // read package.json
-var pkg;
+let pkg;
 try {
-  pkg = require(process.cwd() + "/package.json");
-} catch (e) {
+  pkg = JSON.parse(fs.readFileSync(process.cwd() + "/package.json", "utf8"));
+} catch {
   throw "No package.json found in " + process.cwd();
 }
 
 // read changelog
-var changelog;
+let changelog;
 try {
   changelog = fs.readFileSync(process.cwd() + "/" + changelogFileName, {
-    encoding: "utf8"
+    encoding: "utf8",
   });
-} catch (e) {
+} catch {
   throw "No " + changelogFileName + " found in " + process.cwd();
 }
 
 // parse repository url to get user & repo slug
-var repoUrl;
-repoUrl = pkg.repository;
+let repoUrl = pkg.repository;
 if (repoUrl === undefined) {
   throw "No repository.url found in " + process.cwd() + "/repository(.url)";
 }
 if (typeof repoUrl === "object" && repoUrl.url) {
   repoUrl = repoUrl.url;
 }
-var matches = repoUrl.match(
-  /(?:https?|git(?:\+ssh)?)(?::\/\/)(?:www\.)?github\.com\/(.*)/i
-);
+const matches = repoUrl.match(/(?:https?|git(?:\+ssh)?)(?::\/\/)(?:www\.)?github\.com\/(.*)/i);
 if (matches === null) {
   throw "Unable to parse repository url";
 }
-var repoData = matches[1].split("/");
-var user = repoData[0];
-var repo = repoData[1].replace(/\.git$/, "");
+const repoData = matches[1].split("/");
+const user = repoData[0];
+const repo = repoData[1].replace(/\.git$/, "");
 
 // version
-var version = pkg.version;
+const version = pkg.version;
 
 // Look for the tag in either X.Y.Z or vX.Y.X formats
-var tags = cp.execSync("git tag", { encoding: "utf8" });
-var tagMatches = tags.match(new RegExp("^(v?)" + version + "$", "gm"));
-var tagName;
+const tags = cp.execSync("git tag", { encoding: "utf8" });
+const tagMatches = tags.match(new RegExp("^(v?)" + version + "$", "gm"));
+let tagName;
 if (tagMatches === null) {
   throw "Tag " + version + " or v" + version + " not found";
 } else {
@@ -107,70 +102,69 @@ if (tagMatches === null) {
 }
 
 // changelog
-var body = [];
-var start = false;
-var changelogLines = changelog.replace(/\r\n/g, "\n").split("\n");
+const body = [];
+let start = false;
+const changelogLines = changelog.replace(/\r\n/g, "\n").split("\n");
 
 // accept various ways to specify version starting like
 // # 1.0
 // ## v1.0
 // ## [v1.0
-var versionStartStringRe = "^##? \\[?v?";
-var versionStartRe = new RegExp(versionStartStringRe);
-var versionRe = new RegExp(versionStartStringRe + version.replace(/\./, "."));
-var footerLinkRe = new RegExp("^\\[");
+const versionStartStringRe = "^##? \\[?v?";
+const versionStartRe = new RegExp(versionStartStringRe);
+const versionRe = new RegExp(versionStartStringRe + version.replace(/\./, "."));
+const footerLinkRe = new RegExp("^\\[");
 
-changelogLines.some(function(line, i) {
-  argv.debug && console.log("MATCH", line.match(versionRe));
+changelogLines.some(function (line) {
+  if (argv.debug) console.log("MATCH", line.match(versionRe));
   if (!start && line.match(versionRe)) {
     start = true;
-    argv.debug && console.log("START");
-  } else if (
-    start &&
-    (line.match(versionStartRe) || line.match(footerLinkRe))
-  ) {
-    argv.debug && console.log("END");
+    if (argv.debug) console.log("START");
+  } else if (start && (line.match(versionStartRe) || line.match(footerLinkRe))) {
+    if (argv.debug) console.log("END");
     return true;
   } else if (start) {
-    argv.debug && console.log(line);
+    if (argv.debug) console.log(line);
     // between start & end, collect lines
     body.push(line);
   }
-  argv.debug && console.log("IGNORED " + line);
+  if (argv.debug) console.log("IGNORED " + line);
 });
-body = body.join("\n").trim();
+const releaseBody = body.join("\n").trim();
 
 // prepare release data
-var releaseOptions = {
+const releaseOptions = {
   user: user,
   repo: repo,
   tag: tagName,
   name: tagName,
-  body: body
+  body: releaseBody,
 };
 
-var githubReleaseUrl =
-  "https://github.com/" + user + "/" + repo + "/releases/tag/" + tagName;
+const githubReleaseUrl = "https://github.com/" + user + "/" + repo + "/releases/tag/" + tagName;
 
 if (argv.dryRun) {
   console.log(tagName);
   console.log();
   console.log("---");
-  console.log(body);
+  console.log(releaseBody);
   console.log("---");
   console.log();
   console.log("NOT released, link below should not have the release notes");
   console.log(githubReleaseUrl);
 } else {
-  var token = process.env.GITHUB_TOKEN;
+  const token = process.env.GITHUB_TOKEN;
   if (!token) {
     throw "GITHUB_TOKEN required";
   }
 
-  release(token, releaseOptions, function(err) {
-    if (err) {
-      throw err;
-    }
-    console.log(githubReleaseUrl);
-  });
+  grizzly(token, releaseOptions).then(
+    function () {
+      console.log(githubReleaseUrl);
+    },
+    function (err) {
+      console.error(err && err.message ? err.message : err);
+      process.exit(1);
+    },
+  );
 }
